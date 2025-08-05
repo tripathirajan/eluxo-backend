aa7f47820c87496fa16efc56667db1bbdd21d817
Aditya Tripathi <aditya.tripathi.in@gmail.com>
2025-08-05 21:22:29 +0530

    style(formatting): Changed formate

--- server/plugins/globalErrorHandler.js
+++ server/plugins/globalErrorHandler.js
@@ -1,9 +1,9 @@
 const useGlobalErrorHandler = (app) => {
-    console.log('initiating global error handler');
-    app.use((err, req, res, next) => {
-        console.warn('ResError', err);
-        res.error(err);
-    });
+  console.log('initiating global error handler');
+  app.use((err, req, res, next) => {
+    console.warn('ResError', err);
+    res.error(err);
+  });
 };
 
 module.exports = useGlobalErrorHandler;
